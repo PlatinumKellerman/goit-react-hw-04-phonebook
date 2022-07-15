0d2efc1cc08ccbe46552e22dba568b76494e9fed
PlatinumKellerman <platinumm@ukr.net>
2022-07-15 15:15:39 +0300

    in-progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,22 @@ import { Container } from './components/Container/index';
 import { PhonebookForm } from './components/PhonebookForm/index';
 import { Contacts } from 'components/Contacts/index';
 import { Filter } from './components/Filter/index';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import localContacts from './data/localContacts.json';
+import { useLocalStorage } from './hooks/useLocalStorage';
+import { LOCAL_STORAGE_KEY } from './KEY/localStorageKey';
 
 export function App() {
+  const [contacts, setContacts] = useLocalStorage(
+    LOCAL_STORAGE_KEY,
+    localContacts
+  );
   const [filter, setFilter] = useState('');
-
-  function componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    if (contacts) {
-      this.setState({ contacts });
-    }
-  }
-
-  function componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  console.log(contacts);
 
   const contactHandler = data => {
-    const { contacts } = this.state;
     const findContact = contacts.find(contact => contact.name === data.name);
+    console.log(data);
     if (findContact) {
       alert(`${data.name} is already in contact`);
     } else {
@@ -31,53 +26,39 @@ export function App() {
         id: nanoid(),
         ...data,
       };
-      this.setState({
-        contacts: [...contacts, contact],
-      });
+      setContacts(prev => [contact, ...prev]);
     }
   };
 
-  const handleSearchChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+  const handleSearchChange = event => {
+    setFilter(event.target.value);
   };
 
   const onFilteredContacts = value => {
     const filterNormalize = value.toLowerCase();
-    if (this.state.contacts) {
-      return this.state.contacts.filter(contact => {
-        return contact.name.toLowerCase().includes(filterNormalize);
-      });
-    }
+    return contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filterNormalize);
+    });
   };
 
   const onContactDelete = id => {
-    const { contacts } = this.state;
-    if (contacts) {
-      const contactToDelete = contacts.filter(contact => {
-        return contact.id !== id;
-      });
-      this.setState({
-        contacts: contactToDelete,
-      });
-    }
+    setContacts(prev => prev.filter(contact => contact.id !== id));
   };
 
-  // const { filter } = this.state;
   return (
     <Container>
       <h1>Phonebook</h1>
-      <PhonebookForm onSubmit={this.contactHandler} title="Phonebook" />
+      <PhonebookForm onSubmit={contactHandler} title="Phonebook" />
       <Filter
         value={filter}
         title="Find contacts by name:"
-        onChange={this.handleSearchChange}
+        onChange={handleSearchChange}
       />
       <h2>Contacts</h2>
       <Contacts
         title="Contacts"
-        filteredContacts={this.onFilteredContacts(filter)}
-        onContactDelete={this.onContactDelete.bind(this)}
+        filteredContacts={onFilteredContacts(filter)}
+        onContactDelete={onContactDelete}
       />
     </Container>
   );
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,7 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export const useLocalStorage = (defaultValue, key) => {
+export const useLocalStorage = (key, defaultValue) => {
   const [state, setState] = useState(() => {
     return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
   });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
 };
